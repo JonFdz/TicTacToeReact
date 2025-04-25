181e080e0a4fc7262cd0f4dee7539e7c6a2ff375
JonFdz <jonfernal@gmail.com>
2025-04-25 08:25:34 +0200

    Integrated Player Component to main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Player from './components/Player.jsx'
 
 function App() {
 
 	return (
-		<></>
+		<main>
+			<div id="game-container">
+				<ol id="players">
+					<li className="player">
+						<Player initialName="Player 1" symbol="X" />
+					</li>
+					<li className="player">
+						<Player initialName="Player 2" symbol="O" />
+					</li>
+				</ol>
+			</div>
+		</main>
 	)
 }
 
